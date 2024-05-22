08340da6e74c9999ea0607e1caf652defd59a150
Systems Engineer <79290431+enrojass04@users.noreply.github.com>
2024-05-22 13:36:26 -0500

    Delete src directory

--- src/service/SignupService.jsx
+++
@@ -1,9 +0,0 @@
-const API_URL = `${import.meta.env.VITE_API_URL}/user`;
-
-export const signupUser = async (data) => {
-    return await fetch(API_URL + "/register", {
-        method: "POST",
-        headers: { 'Content-Type': 'application/json', },
-        body: JSON.stringify({  "firstname":data.firstname, "email":data.email, "mobile":data.mobile, "password":data.password }), 
-    })
-}
